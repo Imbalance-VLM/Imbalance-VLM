a39b33d7bb5552d086468e1f74a9962b04c774ec
Hao <39571115+Hhhhhhao@users.noreply.github.com>
2023-03-17 10:33:44 -0400

    Update places.py

--- imblearn/datasets/cv_datasets/places.py
+++ imblearn/datasets/cv_datasets/places.py
@@ -24,7 +24,6 @@ RGB_statistics = {
 def get_data_transform(split, rgb_mean, rbg_std, key='default'):
     data_transforms = {
         'train': transforms.Compose([
-            transforms.RandomResizedCrop(224),
             RandomResizedCropAndInterpolation((224, 224)),
             transforms.RandomHorizontalFlip(),
             RandAugment(3, 10),
